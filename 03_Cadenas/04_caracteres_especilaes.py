46a2c3e301953755e388a7aa3ff4fd14be74af63
yapdever <yapdever@gmail.com>
2025-06-20 14:51:14 -0600

    Modificacion de archivos

---
+++ 03_Cadenas/04_caracteres_especilaes.py
@@ -0,0 +1,27 @@
+# Carateres Especiales
+
+"""
+Las cadenas pueden incluir caracteres especilaes
+
+Estos caracteres se introducen usando el caracter de diagonal invertida (\n) Ej.
+
+- Nueva Linea: '\n' --> Inserta un salto de linea.
+
+- Tabulacion: '\t' --> Inserta un tabulador horizontal, util para alinear el texto
+
+- Comilla Simple: '\'' --> Permite inlcuir comillas suimples en una cadena delimitada
+    por comillas simples.
+
+- Comilla Dobles: '\''' --> Permite inlcuir comillas dobles en una cadena delimitada
+    por comillas dobles.
+
+- Barra INvertida: '\\' --> Permite incluir una barra invertida en la cadena
+
+Existen mas caracteres espciales, pero estos son los presenciales.
+"""
+
+print('Hola \nMundo')  # \n es un salto de linea
+print('\t\tPython es genial')  # \t agrega un tabulador
+print('Juan \' "Perez')
+print("Karla \" Gomez")
+print('Caracyer \\ diagonal invertida')
